89916b6298f8171755556400e9880dc4b88b8620
Falco Winkler <falco.winkler@esailors.de>
2018-08-31 16:32:44 +0000

    added download if not present

---
+++ mouse_autoencoder/input.py
@@ -0,0 +1,26 @@
+import boto3
+import botocore
+import os
+
+dataset_url = "https://github.com/falcowinkler/n-maps-dataset/raw/master/n_maps.tfrecord"
+
+
+def download_if_not_present():
+
+    path_to_file = os.getcwd() + "/PBMC.csv"
+
+    if os.path.isfile(path_to_file):
+        return
+
+    BUCKET_NAME = 'pbmcasingelcell'
+    KEY = 'PBMC.csv'
+
+    s3 = boto3.resource('s3')
+
+    try:
+        s3.Bucket(BUCKET_NAME).download_file(KEY, os.getcwd() + "/PBMC.csv")
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print("The object does not exist.")
+        else:
+            raise
