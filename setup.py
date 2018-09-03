e981e6f0d2a92a7dafa2bf717b2f93b89d725104
Daniel Schruhl <danielschruhl@gmail.com>
2018-09-03 08:04:19 +0200

    add this project as dependency

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='mouse_autoencoder',
+      version='0.1.0',
+      description='',
+      url='https://github.com/DiscoverAI/mouse-autoencoder',
+      author='Daniel Schruhl',
+      author_email='danielschruhl@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
