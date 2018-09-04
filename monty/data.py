a1f1a2d8e79262e74a50723c29cdfe1b16aaaf7a
Falco Winkler <falco.winkler@esailors.de>
2018-09-04 17:15:41 +0000

    read the dataset

--- monty/data.py
+++ monty/data.py
@@ -2,14 +2,14 @@ import logging
 import os
 
 import s3fs
+import tensorflow as tf
 
 logger = logging.getLogger(__name__)
 
 
-def download_if_not_present():
+def download_if_not_present(local_file_path='resources/PBMC.csv'):
     s3bucket = 'pbmcasinglecell/'
     key = 'PBMC.csv'
-    local_file_path = 'resources/PBMC.csv'
 
     if not os.path.isfile(local_file_path):
         logging.info('downloading dataset')
@@ -18,3 +18,18 @@ def download_if_not_present():
 
     logger.info('done downloading dataset')
     return local_file_path
+
+
+def create_dataset(path_to_raw_dataset, num_epochs=10, batch_size=128, shuffle_buffer_size=1000, row_count=2700):
+    filenames = [path_to_raw_dataset]
+    record_defaults = [tf.int64] * row_count
+    dataset = tf.contrib.data.CsvDataset(filenames, record_defaults, header=True,
+                                         select_cols=[x for x in range(1, row_count + 1)])
+    dataset = dataset.shuffle(shuffle_buffer_size)
+    dataset = dataset.batch(batch_size=batch_size, drop_remainder=True)
+    dataset = dataset.repeat(num_epochs)
+    iterator = dataset.make_one_shot_iterator()
+    return iterator.get_next()
+
+
+
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,27 +1,40 @@
 import os
 
+import tensorflow as tf
 from monty import data
+
+test_file_path = 'resources/PBMC_test.csv'
 
 
 def test_download_file_not_present(mocker):
     try:
-        os.remove('resources/PBMC.csv')
+        os.remove(test_file_path)
     except OSError:
         pass
     mocked_s3 = mocker.patch('s3fs.S3FileSystem.get')
 
-    assert data.download_if_not_present() == 'resources/PBMC.csv'
-    mocked_s3.assert_called_once_with('pbmcasinglecell/PBMC.csv', 'resources/PBMC.csv')
+    assert data.download_if_not_present(local_file_path=test_file_path) == test_file_path
+    mocked_s3.assert_called_once_with('pbmcasinglecell/PBMC.csv', test_file_path)
 
 
 def test_download_file_present(mocker):
     try:
-        os.remove('resources/PBMC.csv')
+        os.remove(test_file_path)
     except OSError:
         pass
-    dataset = open('resources/PBMC.csv', 'w')
+    dataset = open(test_file_path, 'w')
     dataset.write(' ')
     mocked_s3 = mocker.patch('s3fs.S3FileSystem.get')
 
-    assert data.download_if_not_present() == 'resources/PBMC.csv'
+    assert data.download_if_not_present(local_file_path=test_file_path) == test_file_path
     assert mocked_s3.call_count == 0
+
+
+def test_create_dataset():
+    iterator = data.create_dataset("test_resources/PBMC_test.csv", batch_size=4, shuffle_buffer_size=8)
+    sess = tf.Session()
+    with sess.as_default():
+        tf.train.start_queue_runners(sess)
+        first_batch = sess.run(iterator)
+        print(first_batch)
+        assert len(first_batch) == 2700
